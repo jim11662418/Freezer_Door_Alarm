fb53c0d5226db49a80bd16fa03e67c5b274624c0
Jim <51113579+jim11662418@users.noreply.github.com>
2020-09-10 09:14:36 -0400

    Update FreezerAlarm.ino

--- FreezerAlarm.ino
+++ FreezerAlarm.ino
@@ -1,3 +1,19 @@
+//---------------------------------------------------------------------------------------------------------------------------------
+// Copyright © 2020 Jim Loos
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files
+// (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge,
+// publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do
+// so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+// OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+// IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//---------------------------------------------------------------------------------------------------------------------------------
+
 //-------------------------------------------------------------------------------------------
 // Freezer Door Monitor 
 // uses a magnetic reed switch to detect when the freezer door is opened.
@@ -14,14 +30,13 @@
 // Version 1.0    initial release
 //         2.0    pin change interrupt replaces pin polling, reworked main loop
 //-------------------------------------------------------------------------------------------
-
 #include <TimeLib.h>
 #include <ESP8266WiFi.h>
-#include <NtpClientLib.h>
-#include <EMailSender.h>
-#include <Ticker.h>
-#include <DallasTemperature.h>
-#include <ThingSpeak.h>
+#include <NtpClientLib.h>           // https://github.com/gmag11/NtpClient
+#include <EMailSender.h>            // https://github.com/xreef/EMailSender
+#include <Ticker.h>                 // https://github.com/esp8266/Arduino/blob/master/libraries/Ticker/src/Ticker.h
+#include <DallasTemperature.h>      // https://github.com/milesburton/Arduino-Temperature-Control-Library
+#include <ThingSpeak.h>             // https://github.com/mathworks/thingspeak-arduino
 
 // WEMOS D1 Mini and NodeMCU pins
 #define D0 16           // blue LED on the left side of NodeMCU
